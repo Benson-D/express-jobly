b352e3afb5e570ccdce66be897b653d56b0783a1
vcheng33 <vanessa.ah.cheng@gmail.com>
2021-10-06 11:36:54 -0700

    completed writing tested for sql.js

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -4,19 +4,33 @@ const { sqlForPartialUpdate } = require("./sql");
 /**  */
 
 describe("Create partial update", function () {
+  const data = {
+    "name": "apple", 
+    "description": "tech company", 
+    "num_employees": "2"};
+
+  const data2 = {};
+  
+  const jsToSql = {
+    "numEmployees": "num_employees"
+  }
+
   test("works, valid object return", function () {
-    const result = sqlForPartialUpdate({
-      name: "",
-      description: "",
-      numEmployees: "num_employees",
-      logoUrl: "logo_url",
-    });
+    const result = sqlForPartialUpdate(data, jsToSql);
 
     expect(result).toEqual({
-      setCol: "name=$1, description=$2, num_employees=$3, logo_url=$4",
-      values: ["num_employees", "logo_url"],
+      setCols: "\"name\"=$1, \"description\"=$2, \"num_employees\"=$3",
+      values: ['apple', 'tech company', '2']
     });
+  });
+
+  test("no data in dataObject, return error", function () {
+    try {
+      const result = sqlForPartialUpdate(data2, jsToSql);
+    } catch (err) {
+      expect (err instanceof BadRequestError).toBeTruthy();
+    }
   });
 });
 
-// {name: dan, description: "music", numEmployees=2, logoUrl=""}
+
