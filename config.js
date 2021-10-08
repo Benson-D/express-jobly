0bbe70399c2ed1a17b4f3143670c3104490376f2
Dan Benson <dbenson3091@gmail.com>
2021-10-08 14:07:13 -0500

    updated key in config

--- config.js
+++ config.js
@@ -5,7 +5,8 @@
 require("dotenv").config();
 require("colors");
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+// const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+const SECRET_KEY = process.env.SECRET_KEY || "i-have-a-secret";
 
 // const PORT = +process.env.PORT || 3001;
 const PORT = process.env.PORT || 3000;
