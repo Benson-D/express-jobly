42958335dd958b0d4e09eb491a1caa08c8f20c80
Dan Benson <dbenson3091@gmail.com>
2021-10-08 15:18:32 -0500

    updated deployment features

--- config.js
+++ config.js
@@ -12,10 +12,17 @@ const SECRET_KEY = process.env.SECRET_KEY || "i-have-a-secret";
 const PORT = process.env.PORT || 3000;
 
 // Use dev database, testing database, or via env var, production database
-function getDatabaseUri() {
-  return process.env.NODE_ENV === "test"
+
+const DB_URI =
+  process.env.NODE_ENV === "test"
     ? "jobly_test"
     : process.env.DATABASE_URL || "jobly";
+
+function getDatabaseUri() {
+  return DB_URI;
+  // process.env.NODE_ENV === "test"
+  //   ? "jobly_test"
+  //   : process.env.DATABASE_URL || "jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
