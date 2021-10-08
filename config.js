812cb0cc0e71d3685942c8932b86d8aecdb68caf
Dan Benson <dbenson3091@gmail.com>
2021-10-08 15:34:54 -0500

    updated deployment info

--- config.js
+++ config.js
@@ -5,24 +5,16 @@
 require("dotenv").config();
 require("colors");
 
-// const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
-const SECRET_KEY = process.env.SECRET_KEY || "i-have-a-secret";
+const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-// const PORT = +process.env.PORT || 3001;
-const PORT = process.env.PORT || 3000;
+const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 
-const DB_URI =
-  process.env.NODE_ENV === "test"
+function getDatabaseUri() {
+  return process.env.NODE_ENV === "test"
     ? "jobly_test"
     : process.env.DATABASE_URL || "jobly";
-
-function getDatabaseUri() {
-  return DB_URI;
-  // process.env.NODE_ENV === "test"
-  //   ? "jobly_test"
-  //   : process.env.DATABASE_URL || "jobly";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
